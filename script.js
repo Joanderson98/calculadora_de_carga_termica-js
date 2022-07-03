c8dbe561dc2489d798888e54c7ac1a99771b7a44
joandrsonjunqueira <joandersonjunqueira@gmail.com>
2022-07-03 16:46:38 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,81 @@
+
+let metrosQ = document.getElementById("metrosQ")
+let QtdPessoas = document.getElementById("QtdPessoas")
+let QtdEletro = document.getElementById("QtdEletro")
+let resultado = document.getElementById("resultado")
+let n = document.getElementById("n")
+
+let parede = document.getElementById("parede")
+
+let btusP = 800;
+let btuS = 600;
+
+
+function calcular(){
+    console.log("testando")
+
+    let Xmetros = metrosQ.value;
+    let Xpessoas = QtdPessoas.value;
+    let Xeletro = QtdEletro.value;
+
+    let n1 = (Xmetros * btuS) + (Xpessoas * check(checkB)) + (Xeletro * check(checkB)); 
+    if(n1 > 60000){
+        n.innerHTML = "Será necessário mais de um aparelho. Por favor, verifique com um técnico da sua confiança."
+    }else if(n1 > 48000){
+        n.innerHTML = "Será necessário um ar de 60000Btus."
+    } else if(n1 > 36000){
+        n.innerHTML = "Será necessário um ar de 48000Btus."
+    } else if(n1 > 30000){
+        n.innerHTML = "Será necessário um ar de 36000Btus."
+    }else if(n1 > 28000){
+        n.innerHTML = "Será necessário um ar de 30000Btus."
+    }else if(n1 > 24000){
+        n.innerHTML = "Será necessário um ar de 28000Btus."
+    }else if(n1 > 18000){
+        n.innerHTML = "Será necessário um ar de 24000Btus."
+    }else if(n1 > 12000){
+        n.innerHTML = "Será necessário um ar de 18000Btus."
+    }else if(n1 > 9000){
+        n.innerHTML = "Será necessário um ar de 12000Btus."
+    }else if(n1 > 7000){
+        n.innerHTML = "Será necessário um ar de 9000Btus."
+    }else if(n1 > 2000){
+        n.innerHTML = "Será necessário um ar de 7000Btus."
+    }else{
+        n.innerHTML = "erro."
+    }
+  
+
+
+}
+
+
+
+let comprimento = document.getElementById('comprimento')
+let largura = document.getElementById('largura')
+let metroq = document.getElementById("Mq")
+
+function calcM(){
+    console.log("Calculando...")
+
+    let larg = largura.value;
+    let comp = comprimento.value;
+
+    let total = larg * comp;
+
+    metroq.innerHTML ="Sua área é equivalente a " + total + " metros quadrados." 
+
+
+}
+
+let checkB = document.getElementById('parede');
+
+checkB.addEventListener('change', check)
+
+function check(checkB){
+    if(checkB.checked == true){
+        return btusP
+    }else{
+        return btuS
+    } 
+}
